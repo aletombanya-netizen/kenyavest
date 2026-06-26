f6c79a2d7e67cc59c49a00a000ec2c13235c1544
Benard <snipedollarfx@gmail.com>
2026-06-26 23:40:13 +0300

    fix: add vertical padding to grad-text to prevent top/bottom clipping mask issue

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    Provides offline support and asset caching
    ===================================================== */
 
-const CACHE_NAME = 'kenyavest-v7';
+const CACHE_NAME = 'kenyavest-v8';
 const OFFLINE_URL = '/';
 
 // Assets to cache immediately on install
