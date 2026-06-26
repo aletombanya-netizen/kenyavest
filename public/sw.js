d467396914b0ed0ff3afa7b4046006b135b3fcd2
Benard <snipedollarfx@gmail.com>
2026-06-26 20:12:29 +0300

    Bump service worker cache to v3 to force reload of dashboard PIN UI

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    Provides offline support and asset caching
    ===================================================== */
 
-const CACHE_NAME = 'kenyavest-v1';
+const CACHE_NAME = 'kenyavest-v3';
 const OFFLINE_URL = '/';
 
 // Assets to cache immediately on install
