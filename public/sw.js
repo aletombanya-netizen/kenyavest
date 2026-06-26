004ef44a16f850adc950b9f17776d8d82a83b0cc
Benard <snipedollarfx@gmail.com>
2026-06-27 00:06:19 +0300

    fix: make navbar and ticker backgrounds completely transparent to prevent masking

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    Provides offline support and asset caching
    ===================================================== */
 
-const CACHE_NAME = 'kenyavest-v10';
+const CACHE_NAME = 'kenyavest-v11';
 const OFFLINE_URL = '/';
 
 // Assets to cache immediately on install
